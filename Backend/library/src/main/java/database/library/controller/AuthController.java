ea1034850c2e04e0d16ef6ea114192220354bd0f
ANGEL SOLANO <agsolano-2023a@corhuila.edu.co>
2025-05-27 23:39:06 -0500

    prueba

--- Backend/library/src/main/java/database/library/controller/AuthController.java
+++ Backend/library/src/main/java/database/library/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins ="*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -15,7 +15,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @PostMapping("/login")
+    @PostMapping("/login.")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         AuthResponse response = authService.verificarCredenciales(request);
         return response != null ? ResponseEntity.ok(response) : ResponseEntity.status(401).build();
